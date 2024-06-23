f3f18ac4f4470f916db9a19c888627d7a4d869b1
europokefreak <juan_velazquezg@hotmail.com>
2024-06-23 18:53:09 +0200

    Añadir cambios de segunda iteracion

--- src/js/rendimiento.js
+++ src/js/rendimiento.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const iframeContainer = document.getElementById('video-placeholder');
-    const iframe = document.createElement('iframe');
-    iframe.width = "560";
-    iframe.height = "315";
-    iframe.src = "https://www.rtve.es/play/videos/alaska-y-segura/alaska-segura-bistecs-historia-del-arte/3048186/";
-    iframe.title = "Video de Alaska y Segura";
-    iframe.allow = "encrypted-media; web-share";
-    iframe.allowFullscreen = true;
-    iframe.loading = "lazy";
-    iframeContainer.appendChild(iframe);
+    let video = document.getElementById('myVideo');
+
+    let observer = new IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                video.load();
+                observer.unobserve(video);
+            }
+        });
+    });
+
+    observer.observe(video);
 });
